e9c36735bb9c0c0bd7c154c6424117c06423c10f
liky1516 <ksav2003@inbox.ru>
2023-05-12 00:38:38 +0300

    Update Console_Battleship_v2.py

--- Console_Battleship_v2.py
+++ Console_Battleship_v2.py
@@ -58,7 +58,8 @@ class Ship:
 		self.lives = size
 
 	def get_ship_dots(self):
-		return self.dots
+		for i in range(self.dots):
+			return self.dots
 
 	def ship_dots(self):
 		for i in range(self.size):
@@ -67,11 +68,14 @@ class Ship:
 
 
 class Field:
-	def __init__(self):
+	def __init__(self, hid=True, living_ships=7):
+		self.hid = hid
+		self.living_ships = living_ships
 		self.cells = [[Dot(x, y, state=CellState.EMPTY) for x in range(1, 7)] for y in range(1, 7)]
 		self.ships = Field.list_ships(self)
 		for i in self.ships:
 			i.ship_dots()
+
 
 	def list_ships(self):
 		return [
@@ -93,8 +97,19 @@ class Field:
 			print(row_str)
 			print('    _________________________')
 
-	def add_ship(self, ship):
-		pass
+	def add_ship(self):
+		for dot in self.ships:
+			# if (dot.y - 1) < 1 or (dot.y - 1) > 6 \
+			# 	or (dot.x - 1) < 1 or (dot.x - 1) > 6:
+			# 	raise ValueError ('Invalid ship location')
+			# if dot.turn == 0 and dot.x + (dot.size - 1) > 6:
+			# 	raise ValueError("Ship goes out of bounds")
+			# if dot.turn == 1 and dot.y + (dot.size - 1) > 6:
+			# 	raise ValueError("Ship goes out of bounds")
+
+			self.cells[dot.y - 1][dot.x - 1].state = CellState.SHIP
+
+
 
 
 # ________________________________________________________________
@@ -105,7 +120,11 @@ class Field:
 
 if __name__ == "__main__":
 	fil = Field()
+	fil.add_ship()
 	fil.show_field()
+	# fil.add_ship()
+
+
 
 
 # s = Ship(1, 1, 3, 0)
